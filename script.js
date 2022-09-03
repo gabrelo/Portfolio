acd1148b453907945ef2f107e15c6773182cfd0a
Gabriel do Carmo Sousa <gabriel10moines@gmail.com>
2022-09-03 16:50:03 -0300

    ultimas atts

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let parent = document.querySelectorAll('.animate-text');
 for(let i = 0; i < parent.length; i++) {
   parent[i].style.width = parent[i].children[0].clientWidth + "px"; 
 };
+
+var btn = document.querySelector("#back-to-top");
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
